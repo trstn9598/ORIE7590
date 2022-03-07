3f784d68c403e1540ad1e7839a64f3dd1bcbca4f
Tonghua Tian <tt543@cornell.edu>
2022-03-06 19:10:34 -0500

    Modified setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(ext_modules = cythonize(Extension(
     'bd_sim_cython',
     sources=['bd_sim_cython.pyx'],
     language='c++',
-    extra_compile_args=['-std=c++17', '-O3', '-fopenmp'],
+    extra_compile_args=['-std=c++17', '-O3', "-Xpreprocessor", '-fopenmp'],
     extra_link_args=['-lomp']
     #include_dirs=[numpy.get_include()],
     #library_dirs=[],
