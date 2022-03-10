a79ef877e00089e9cd5006b7b336f587ce382767
Andrew <andrewchee135@gmail.com>
2022-03-09 23:08:33 -0500

    test update 3/9 v1

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.special import jv, laguerre, poch
 from math import comb, factorial, exp, sqrt, log
 
-def MC_BESQ_gateway(N = 10**6, t = 0, x0 = 0, test = 'Bessel', args = []):
+def MC_BESQ_gateway(N = 10**6, t = 0, x0 = 0, test = 'bessel', args = []):
 	"""
 	Monte Carlo estimator of expected BESQ against test functions
 	:param N: int, Number of simulations
@@ -12,7 +12,7 @@ def MC_BESQ_gateway(N = 10**6, t = 0, x0 = 0, test = 'Bessel', args = []):
 	:param test: defines test function
 	:args: arguments to define test function
 	"""
-	if test == 'Bessel':
+	if test == 'bessel':
 		f = lambda n : laguerre(n)(1)
 	elif test == 'poly':
 		if len(args) < 1:
@@ -26,10 +26,10 @@ def MC_BESQ_gateway(N = 10**6, t = 0, x0 = 0, test = 'Bessel', args = []):
 
 	def poisson_x0():
 		return np.random.poisson(x0)
-	xt_array = BD_simulator.bd_simulator(t, x0=poisson_x0, num_paths=N, method = 'Bessel', num_threads=4)
+	xt_array = BD_simulator.bd_simulator(t, x0=poisson_x0, num_paths=N, method = 'bessel', num_threads=4)
 	return np.mean(np.vectorize(f)(xt_array))
 
-def MC_BESQviaLaguerre_gateway(N = 10**6, t = 0, x0 = 0, test = 'Bessel', args = []):
+def MC_BESQviaLaguerre_gateway(N = 10**6, t = 0, x0 = 0, test = 'bessel', args = []):
 	"""
 	Test Bessel BD simulator against exact formulas for Bessel functions
 
@@ -37,7 +37,7 @@ def MC_BESQviaLaguerre_gateway(N = 10**6, t = 0, x0 = 0, test = 'Bessel', args =
 	:param T: positive float, Simulation horizon
 	:param x0: initial value of X
 	"""
-	if test == 'Bessel':
+	if test == 'bessel':
 		f = lambda n : laguerre(n)(1 + t)
 	elif test == 'poly':
 		if len(args) < 1:
@@ -51,33 +51,33 @@ def MC_BESQviaLaguerre_gateway(N = 10**6, t = 0, x0 = 0, test = 'Bessel', args =
 
 	def poisson_x0():
 		return np.random.poisson(x0)
-	xt_array = BD_simulator.bd_simulator(t = log(t + 1), x0=poisson_x0, num_paths=N, method = 'Laguerre', num_threads=4)
+	xt_array = BD_simulator.bd_simulator(t = log(t + 1), x0=poisson_x0, num_paths=N, method = 'laguerre', num_threads=4)
 	return np.mean(np.vectorize(f)(xt_array))
 
 
-def estimate_SqBessel(t = 0, x0 = 0, test = 'Bessel', args = []):
+def estimate_SqBessel(t = 0, x0 = 0, test = 'bessel', args = []):
 	# DO NOT USE YET
-	if test == 'Bessel':
-		f = lambda x : exp(-t)*jv(0, 2*np.sqrt(x))
+	if test == 'bessel':
+		fn = lambda x : exp(-t)*jv(0, 2*np.sqrt(x))
 	elif test == 'poly':
 		if len(args) < 1:
 			print('No coefficients provided')
 			coef = []
 		else: 
 			coef = args[0]
-		f = lambda x : np.polyval(coef, x)
+		fn = lambda x : np.polyval(coef, x)
 	else:
-		f = lambda x : 0
-	estimate = f(x0)
+		fn = lambda x : 0
+	estimate = fn(x0)
 	return estimate
 
 def discrete_poly(n, coef):
 	return sum([coef[i]*poch(n - i + 1, i) for i in range(len(coef)) if n >= i])
 
 
-x0 = 1
+x0 = 0
 coef = [1]
 t = 1
-print(estimate_SqBesselBD(N = 10, t = t, x0 = x0, test = 'Bessel', args = []))
+print(MC_BESQ_gateway(N = 10**4, t = t, x0 = x0, test = 'bessel', args = []))
 print(estimate_SqBessel(t = t, x0 = x0))
 
